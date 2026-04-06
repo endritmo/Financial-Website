e258076d0535aa87b944652c70cbaa2b0f18b64c
Endrit Morina <endritm167@gmail.com>
2026-04-06 20:18:45 +0200

    charts overhaul

--- core/views.py
+++ core/views.py
@@ -4,8 +4,29 @@ def home(request):
     return render(request, 'home.html')
 
 def charts(request):
-    symbol = request.GET.get('symbol', 'BINANCE:BTCUSDT')
-    return render(request, 'charts.html', {'symbol': symbol})
+    PRESET_SYMBOLS = [
+        'BINANCE:BTCUSDT','BINANCE:ETHUSDT','BINANCE:BNBUSDT','BINANCE:SOLUSDT','BINANCE:XRPUSDT',
+        'OANDA:EURUSD','OANDA:USDJPY','OANDA:GBPUSD','OANDA:USDCHF','OANDA:AUDUSD',
+        'NASDAQ:AAPL','NASDAQ:TSLA','NASDAQ:MSFT','NASDAQ:AMZN','NASDAQ:NVDA',
+        'TVC:GOLD','TVC:SILVER','TVC:USOIL','TVC:UKOIL',
+        'OANDA:NAS100USD','OANDA:SPX500USD','OANDA:US30USD','OANDA:JP225USD','OANDA:UK100GBP'
+    ]
+    preset = request.GET.get("preset_symbol")
+    search = request.GET.get("symbol")
+
+    if preset:
+        symbol = preset
+    elif search:
+        symbol = search
+    else:
+        symbol = "BINANCE:BTCUSDT"
+    # Only show search value if user typed something
+    if search and search not in PRESET_SYMBOLS:
+        search_value = search
+    else:
+        search_value = ""
+    is_custom = symbol not in PRESET_SYMBOLS
+    return render(request, 'charts.html', {'symbol': symbol, 'is_custom': is_custom, 'search_value': search_value})
 
 def calendar(request):
     events = [
